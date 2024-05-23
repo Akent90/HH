1e2b51cd1009f9ff7647f32e08daddda4390aefb
akent <alexanderkent099@gmail.com>
2024-05-23 18:47:12 -0400

    Adding search bar component

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { SearchProvider, WithSearch } from "@elastic/react-search-ui";
+import { SearchBox, Results, Paging } from "@elastic/react-search-ui-views";
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import { config } from "../config/elasticsearch";
+
+const SearchBar = () => (
+  <SearchProvider config={config}>
+    <WithSearch mapContextToProps={({ wasSearched, results }) => ({ wasSearched, results })}>
+      {({ wasSearched, results }) => (
+        <div>
+          <SearchBox autocompleteSuggestions={true} />
+          {wasSearched && (
+            <div>
+              <Results titleField="name" descriptionField="description" />
+              <Paging />
+            </div>
+          )}
+        </div>
+      )}
+    </WithSearch>
+  </SearchProvider>
+);
+
+export default SearchBar;
+
