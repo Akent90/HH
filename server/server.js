58edc6be35a7c2668f09daa3505f7922ea3731fb
akent <alexanderkent099@gmail.com>
2024-05-25 13:48:07 -0400

    Adding logging and express rate limiting to the server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -7,15 +6,15 @@ const { typeDefs, resolvers } = require('./schemas');
 const connectDB = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 const rateLimit = require('express-rate-limit');
-const logger = require('./config/logger'); 
+const logger = require('./config/logger');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startApolloServer = async () => {
   try {
-    await connectDB(); 
-    console.log("Database connected successfully.");
+    await connectDB();
+    logger.info("Database connected successfully.");
 
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
@@ -27,7 +26,7 @@ const startApolloServer = async () => {
     });
     app.use(limiter);
 
-    // Logging
+    // Logging middleware
     app.use((req, res, next) => {
       logger.info(`${req.method} ${req.url}`);
       next();
@@ -41,31 +40,32 @@ const startApolloServer = async () => {
     const server = new ApolloServer({
       typeDefs,
       resolvers,
-      context: ({ req }) => ({ user: req.user }) 
+      context: ({ req }) => ({ user: req.user })
     });
 
     await server.start();
     server.applyMiddleware({ app, path: '/graphql' });
 
     app.use('/graphql', (req, res, next) => {
-      console.log('Incoming GraphQL request:', req.body);
+      logger.info('Incoming GraphQL request:', req.body);
       next();
     });
 
     app.use(express.static(path.join(__dirname, '../client/build')));
-    
+
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
 
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      logger.info(`API server running on port ${PORT}!`);
+      logger.info(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   } catch (err) {
-    console.error("Failed to start server:", err);
+    logger.error("Failed to start server:", err);
   }
 };
 
 startApolloServer();
+
 
